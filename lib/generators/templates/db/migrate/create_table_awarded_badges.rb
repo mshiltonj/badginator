4081c8ae459606ce8dad979d07d1b74c5df188f9
Steven Hilton <mshiltonj@gmail.com>
2013-09-10 21:37:16 -0400

    add rails generator for model & migration

---
+++ lib/generators/templates/db/migrate/create_table_awarded_badges.rb
@@ -0,0 +1,12 @@
+class CreateTableAwardedBadges < ActiveRecord::Migration
+  def change
+    create_table :awarded_badges do |t|
+      t.integer :awardee_id  
+      t.string  :awardee_type  
+      t.string  :badge_code
+      t.timestamps
+    end
+
+    add_index :awarded_badges, [:awardee_id, :awardee_type]
+  end
+end
