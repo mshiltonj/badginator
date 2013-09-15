3551a5e4df71d64f7687c56a07ec7df18ad2cc88
Steven Hilton <mshiltonj@gmail.com>
2013-09-14 21:11:04 -0400

    ensure awardee.badge uses symbols to look up badge

--- lib/generators/templates/app/models/awarded_badge.rb
+++ lib/generators/templates/app/models/awarded_badge.rb
@@ -2,7 +2,7 @@ class AwardedBadge < ActiveRecord::Base
   belongs_to :awardee, polymorphic: true
 
   def badge
-    Badginator.get_badge(badge_code)
+    Badginator.get_badge(badge_code.to_sym)
   end
 
 end
